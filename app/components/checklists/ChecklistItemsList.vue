7e5e7ceaddd602c98063ad8c03aeedb7d1ce59c6
Pier Dolique <perdgit.uieog@slmail.me>
2024-08-06 22:35:27 +0300

    feat: Show checklist items
    Fixes #61

---
+++ app/components/checklists/ChecklistItemsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="$style.component">
+    <ChecklistItem
+      v-for="item in items"
+      :key="item.id"
+      :item="item"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import type { ChecklistItemModel } from '~/models/checklist';
+  import ChecklistItem from './ChecklistItem.vue';
+
+  interface Props {
+    readonly items: ChecklistItemModel[];
+  }
+
+  defineProps<Props>();
+</script>
+
+<style module>
+  .component {
+    width: 100%;
+    display: grid;
+    row-gap: var(--spacing-4);
+  }
+</style>
--- app/composables/use-checklist-items-data.ts
+++ app/composables/use-checklist-items-data.ts
@@ -1,34 +1,12 @@
-interface EquipmentItem {
-  readonly id: string;
-  readonly name: string;
-  readonly weight: number;
-}
-
-interface ChecklistItem {
-  readonly id: string;
-  readonly equipment: EquipmentItem;
-}
+import type { ChecklistItemModel } from "~/models/checklist"
 
 export async function useChecklistItemsData(checklistId: string) {
-  const items = ref<ChecklistItem[]>([])
+  const items = ref<ChecklistItemModel[]>([])
   const { data } = await useFetch(`/api/checklists/${checklistId}/items`)
 
   if (data.value !== undefined) {
     items.value = data.value
   }
 
-  async function updateItems() {
-    try {
-      const response = await $fetch(`/api/checklists/${checklistId}/items`)
-
-      items.value = response
-    } catch {
-      console.error('Failed to update equipment')
-    }
-  }
-
-  return {
-    items,
-    updateItems
-  }
+  return items
 }
